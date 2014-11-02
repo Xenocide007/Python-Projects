575504f6ea32742c5eea58b2d563b3329f64b53b
Jason <shrackx@gmail.com>
2014-11-02 06:25:59 -0700

    Saturday's work
    lots of improvements, just look at the code

--- hangman.py
+++ hangman.py
@@ -1,5 +1,7 @@
 import random
 
+
+
 word = ""
 let = ""
 a = []
@@ -10,15 +12,18 @@ def difficulty():
 	if dif == "1":
 		lives = 10
 		print "You have selected difficulty 1 (10 lives)\n"
+		return lives
 	elif dif == "2":
 		lives = 7
 		print "You have selected difficulty 2 (7 lives\n"
+		return lives
 	elif dif == "3":
 		lives = 3
 		print "WATCH OUT, WE'VE GOT A BOSS OVER HERE!! (3 lives)\n"
+		return lives
 	else:
 		print "Please type only 1 number between 1 and 3\n"
-	return lives
+		exit()
 
 
 def random_word():
@@ -31,13 +36,6 @@ def guess():
 	letter = raw_input("Guess a letter\n")
 	return letter
 
-
-def word_slots(word):
-	for l in word:
-		slots.append("_ ")
-	print slots
-
-
 def replace(word, letter, answer):
 	for l in range(len(word)):
 		if letter.lower() == word[l].lower():
@@ -46,19 +44,24 @@ def replace(word, letter, answer):
 
 def get_slots(word):
 	a = list(word)
-	print a
 	for l in range(len(a)):
-		a[l] = "_ "
-	print a
+		a[l] = '_ '
+	print ''.join(a)
 	return a
 
 
-#does this work?
 
 
-print "HANGMAN"
+print """   		HANGMAN
+           _______
+          |/      |
+          |      (_)
+          |      \|/
+          |       |
+          |      / |
+          |
+         _|___"""
 word = random_word()
-print word
 a = get_slots(word)
 life = difficulty()
 print life
@@ -66,13 +69,11 @@ while True:
 	let = guess()
 	if let in word:
 		replace(word, let, a)
-		print a
+		print ''.join(a)
 	else:
 		if life > 0:
 			life -= 1
-			print "You have %n lives left" % life
+			print "You have %d lives left" % life
 		else:
 			print "You lose! :C"
 			break
-
-
