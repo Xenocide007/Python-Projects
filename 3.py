5371bfd1cad06bd8836a7c813991a658b3fb2d8f
Jason <shrackx@gmail.com>
2014-11-02 15:41:04 -0700

    Renamed and added

---
+++ 3.py
@@ -0,0 +1,8 @@
+#3 in a row
+
+def inputNames():
+	name1 = raw_input("Enter first player's name here: \t")
+	name2 = raw_input("Enter second player's name here: \t")
+	return name1, name2
+
+inputNames()
