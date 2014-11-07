c1a34e68239560ce29773470c8c789342eb78dca
Jason Reynolds <shrackx@gmail.com>
2014-11-06 21:42:54 -0700

    Added who goes first

--- 3.py
+++ 3.py
@@ -1,16 +1,23 @@
 #3 in a row
 import random
-
+names = []
 def inputNames():
 	name1 = raw_input("Enter first player's name here: \t")
 	name2 = raw_input("Enter second player's name here: \t")
 	return name1, name2
 
 def first(name1, name2):
-	num1 = raw_input("%s, guess a number between 1 and 10, closest person wins and get to go first!\t" % name1)
-	num2 = raw_input("%s, pick a number\t" % name2)
+	num1 = int(raw_input("%s, guess a number between 1 and 10, closest person wins and get to go first!\t" % name1))
+	num2 = int(raw_input("%s, pick a number\t" % name2))
 	if num2 == num1:
-		num2 = raw_input("%s, choose a different number" % name2)
+		num2 = int(raw_input("%s, choose a different number" % name2))
 	RNG = random.randint(0, 10)
 	print ("%d is the number" % RNG)
+	if abs(num1 - RNG) > abs (num2 - RNG):
+		print "%s goes first" % name2
+	else:
+		print "%s goes first" % name1
+
+names = inputNames()
+first(names[0], names[1])
 	
