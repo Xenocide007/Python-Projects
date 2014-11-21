64995e281becf297d7748940913c909f205e898d
Jason Reynolds <shrackx@gmail.com>
2014-11-20 21:32:26 -0700

    even more stuffs
    figured out how to make and call values from lists of lists, working on
    replacing said value

--- 3.py
+++ 3.py
@@ -1,7 +1,7 @@
 #3 in a row
 import random
 names = []
-gameBoard = []
+gameBoard = [["1", "2", "3"], ["4", "5", "6"], ["7", "8", "9"]]
 def inputNames():
 	name1 = raw_input("Enter first player's name here: \t")
 	name2 = raw_input("Enter second player's name here: \t")
@@ -19,15 +19,21 @@ def first(name1, name2):
 	else:
 		print "%s goes first" % name1
 
-def getBoard(board):
-	board = [[1, 2, 3], [4, 5 , 6], [7, 8, 9]
-	print board[1]
+def move(board):
+	mov = raw_input("Pick a number between 1 and 9 (that hasn't been picked already)")
+	for listt in board:
+		for subvalue in range(len(listt)):
+			if mov == subvalue:
+				 listt[subvalue]= "X"
+				 print board
+
 
 
 
 
 #names = inputNames()
 #first(names[0], names[1])
-getBoard(gameBoard)
+move(gameBoard)
+
 
 	
